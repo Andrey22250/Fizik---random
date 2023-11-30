4e9f0bc6270987d2beeb0c68772e4aa9b9c1e3c9
Andrey22250 <andrchepras@mail.ru>
2023-12-01 00:22:22 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,48 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <conio.h>
+#define N 100
+
+void clean()
+{
+	while (getchar() != '\n');
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	do
+	{
+		float s = 0, t[N], ts = 0;
+		int i, n;
+		do
+		{
+			printf("Введите кол-во членов: ");
+			scanf("%d", &n);
+			clean();
+		} while (n<1 || n>100);
+		for (i = 0; i < n; i++)
+		{
+			printf("Введите %d член: ", i+1);
+			scanf("%f", &t[i]);
+			clean();
+		}
+		for (i = 0; i < n; i++)
+		{
+			ts = ts + t[i] / n;
+		}
+		ts = ts * 10000;
+		ts = int(ts);
+		ts = ts / 10000;
+		printf("Среднее значение: %g", ts);
+		for (i = 0; i < n; i++)
+		{
+			s = pow((ts - t[i]), 2.0) + s;
+		}
+		s = s / n / (n - 1);
+		s = sqrt(s);
+		printf("\nДоверительный интервал: %g", s);
+		printf("\n\nНажмите Enter для повторного запуска программы\nНажмите Esc для выхода из программы\n\n");
+	}while (_getch()!=27);
+}
